fb92f42c39362f71ab1a96dbd2deefc5aaa41bdd
Francisco Penedo <fran@franpenedo.com>
2020-07-09 18:13:10 -0400

    Added better config, item urls from function result
    Config file can now be passed as argument.
    Config can now have optional 'options' section with output
    directory option.
    Fixed date timezone.
    Item url can be generated using a custom function if it's not
    given as href attribute of an 'a' element.

--- rssify.py
+++ rssify.py
@@ -4,9 +4,25 @@ import requests
 from feedgen.feed import FeedGenerator
 from bs4 import BeautifulSoup
 from pytz import timezone
+from tzlocal import get_localzone
+import os
+import urlparse
+import sys
+
+if len(sys.argv) > 1:
+    config_fn = sys.argv[1]
+else:
+    config_fn = 'config.ini'
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read(config_fn)
+
+try:
+    directory = config.get('options', 'directory')
+except:
+    directory = "."
+
+config.remove_section('options')
 
 for section in config.sections():
     s = dict(config.items(section))
@@ -31,9 +47,16 @@ for section in config.sections():
 
         fe = fg.add_entry()
         fe.title(titles[i].text)
-        fe.link(href=urls[i].get('href'), rel='alternate')
+        if 'item_url_function' in s:
+	     url = urls[i]
+   	     href = eval(s['item_url_function'])
+	else:
+	     href = urls[i].get('href')
+        link = urlparse.urljoin(s['url'], href)
+        fe.link(href=link, rel='alternate')
         if dates is not None:
             date = datetime.strptime(dates[i].text.strip(), s['item_date_format'])
+            date = date.replace(tzinfo=get_localzone())
             if config.has_option(section, 'item_timezone'):
                 localtz = timezone(s['item_timezone'])        
                 date = localtz.localize(date)
@@ -43,4 +66,4 @@ for section in config.sections():
 
         fe.published(date)
 
-    fg.rss_file(section.replace(' ', '_') + '.xml')
+    fg.rss_file(os.path.join(directory, section.replace(' ', '_') + '.xml'))
