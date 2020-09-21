ad95020f8188d789ae5582d20cd58b06bf0a2fde
Francisco Penedo <fran@franpenedo.com>
2020-09-21 11:26:20 -0400

    Added support for steam curators
    - Titles now have transformation function "item_title_function".
    - Added "item_content" to populate content field for each entry. Content also has
      transformation function "item_content_function".
    - Added support for year-less dates. Since datetime.strptime defaults to year 1900, Feb
      29 must be handled on its own. Defaults to current year.
    - Blackened the whole thing, which probably was a horrible idea to do while making
      changes. If future me is reading this while failing to diff something, I apologize.

--- rssify.py
+++ rssify.py
@@ -8,62 +8,94 @@ from tzlocal import get_localzone
 import os
 import urlparse
 import sys
+import re
 
 if len(sys.argv) > 1:
     config_fn = sys.argv[1]
 else:
-    config_fn = 'config.ini'
+    config_fn = "config.ini"
 
 config = configparser.ConfigParser()
 config.read(config_fn)
 
 try:
-    directory = config.get('options', 'directory')
+    directory = config.get("options", "directory")
 except:
     directory = "."
 
-config.remove_section('options')
+config.remove_section("options")
 
 for section in config.sections():
     s = dict(config.items(section))
-    r = requests.get(s['url'])
-    soup = BeautifulSoup(r.text, 'lxml')
-    titles = soup.select(s['item_title'])
-    urls = soup.select(s['item_url'])
+    r = requests.get(s["url"])
+    soup = BeautifulSoup(r.text, "lxml")
+    titles = soup.select(s["item_title"])
+    urls = soup.select(s["item_url"])
 
-    if 'item_date' in s:
-        dates = soup.select(s['item_date'])
+    if "item_date" in s:
+        dates = soup.select(s["item_date"])
     else:
         dates = None
+
+    if "item_content" in s:
+        contents = soup.select(s["item_content"])
+    else:
+        contents = None
 
     fg = FeedGenerator()
     fg.title(section)
     fg.description(section)
-    fg.link(href=s['url'], rel='alternate')
+    fg.link(href=s["url"], rel="alternate")
 
     for i in range(len(titles)):
         if i > len(urls) - 1:
             break
 
         fe = fg.add_entry()
-        fe.title(titles[i].text)
-        if 'item_url_function' in s:
-	     url = urls[i]
-   	     href = eval(s['item_url_function'])
-	else:
-	     href = urls[i].get('href')
-        link = urlparse.urljoin(s['url'], href)
-        fe.link(href=link, rel='alternate')
+
+        if "item_title_function" in s:
+            title = titles[i]
+            title_text = eval(s["item_title_function"])
+        else:
+            title_text = titles[i].text
+        fe.title(title_text)
+
+        if "item_url_function" in s:
+            url = urls[i]
+            href = eval(s["item_url_function"])
+        else:
+            href = urls[i].get("href")
+        link = urlparse.urljoin(s["url"], href)
+        fe.link(href=link, rel="alternate")
+
+        if contents is not None:
+            if "item_content_function" in s:
+                content = contents[i]
+                content_text = eval(s["item_content_function"])
+            else:
+                content_text = contents[i].text
+            fe.content(content_text)
+
+        now = datetime.now(timezone("Europe/Berlin"))
         if dates is not None:
-            date = datetime.strptime(dates[i].text.strip(), s['item_date_format'])
+            datestr = dates[i].text.strip()
+            try:
+                date = datetime.strptime(datestr, s["item_date_format"])
+            except ValueError:
+                date = datetime.strptime(
+                    str(now.year) + "-" + datestr, s["item_date_format"]
+                )
+            # Set default year to current year
+            if date.year == 1900:
+                date = date.replace(year=now.year)
             date = date.replace(tzinfo=get_localzone())
-            if config.has_option(section, 'item_timezone'):
-                localtz = timezone(s['item_timezone'])        
+            if config.has_option(section, "item_timezone"):
+                localtz = timezone(s["item_timezone"])
                 date = localtz.localize(date)
         else:
-            date = datetime.now(timezone("Europe/Berlin")) 
-            #date = '1970-01-01 00:00:00+02:00'
+            date = now
+            # date = '1970-01-01 00:00:00+02:00'
 
         fe.published(date)
 
-    fg.rss_file(os.path.join(directory, section.replace(' ', '_') + '.xml'))
+    fg.rss_file(os.path.join(directory, section.replace(" ", "_") + ".xml"))
